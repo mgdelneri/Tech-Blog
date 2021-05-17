2dfd72f985c88d9686349a606ef5edc7705142ab
Marina <mgdelneri@gmail.com>
2021-05-17 13:21:13 -0500

    Added a package.json file and installed needed dependencies; Fixed .env to be in .gitignore

--- models/index.js
+++ models/index.js
@@ -1,27 +1,27 @@
 // Finds modules with models for User, Post, and Comment
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
 // Associations between models
 
 Post.belongsTo(User, {
-    foreignKey: 'userId',
-    onDelete: 'CASCADE'
+  foreignKey: "userId",
+  onDelete: "CASCADE",
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'postId',
-    onDelete: 'CASCADE'
+  foreignKey: "postId",
+  onDelete: "CASCADE",
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'userId',
-    onDelete: 'CASCADE'
+  foreignKey: "userId",
+  onDelete: "CASCADE",
 });
 
 module.exports = {
-    User,
-    Comment,
-    Post
+  User,
+  Comment,
+  Post,
 };
