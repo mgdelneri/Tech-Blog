bfc919789312453c11fa543f6d2ede724d91b1c0
Marina <mgdelneri@gmail.com>
2021-05-17 12:57:32 -0500

    Coded index.js in models folder, with associations between models

---
+++ models/index.js
@@ -0,0 +1,27 @@
+// Finds modules with models for User, Post, and Comment
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Associations between models
+
+Post.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'postId',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+});
+
+module.exports = {
+    User,
+    Comment,
+    Post
+};
